67250687bc9cec24cf25ec9deb517c573dd2bbcf
KangChanSong <thdrkdcks@naver.com>
2021-09-10 14:09:13 +0900

    Docker

--- deploy.sh
+++ deploy.sh
@@ -1,32 +1,7 @@
-#!/bin/bash
+docker-compose up -d
 
-echo "============== 프론트엔드 배포 시작 =============="
+chmod +x ./deploy.sh
 
-ZIP_REPOSITORY=/home/ec2-user/zips/front
+./deploy.sh
 
-REPOSITORY=/home/ec2-user/app/front
-PROJECT_NAME=mbti_zip_front
-
-echo "> Build 파일 복사"
-
-cp $ZIP_REPOSITORY/* $REPOSITORY/
-
-echo "> 현재 구동중인 애플리케이션 pid 확인"
-
-CURRENT_PID=$(pgrep -fl mbti_zip_front | grep node | awk '{print $1}')
-
-echo "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
-
-if [ -z "$CURRENT_PID"]; then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
-else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
-fi
-
-echo "> 새 애플리케이션 배포"
-
-cd $REPOSITORY/
-
-nohup npm start &
+docker ps
---
+++ execute-deploy.sh
@@ -0,0 +1,2 @@
+cd /home/ec2-user/docker-image
+./deploy.sh > /dev/null 2> /dev/null < /dev/null &
