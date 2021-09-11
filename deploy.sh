5108717a810bf92172f70ccf1012e138572371cd
KangChanSong <thdrkdcks@naver.com>
2021-09-11 21:02:07 +0900

    빌드까지만 deploy.sh에 추가

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "=========== 프론트엔드 배포 시작 =========="
-echo "> /home/ec2-user/mbti_zip_front 로 이동"
-cd /home/ec2-user/mbti_zip_front
+echo "> /home/ec2-user/deploy/mbti_zip_front 로 이동"
+cd /home/ec2-user/deploy/mbti_zip_front
 echo "> 빌드"
 yarn install
 yarn build
