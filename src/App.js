75c9c249d9fb0de741258d7665562dbd984f333b
KangChanSong <thdrkdcks@naver.com>
2021-09-19 17:24:06 +0900

    원

--- src/App.js
+++ src/App.js
@@ -3,19 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import LayoutContainer from './layout/LayoutContainer';
 import React, { useEffect} from 'react';
-import axios from 'axios';
 import { ContextProvider} from './context/ContextContainer';
 
 const App = () => {
-
-  useEffect(() => {
-    const getCookie = async () => {
-      await axios.get("/api/v1/cookie/get");
-    }
-
-    getCookie();
-  },[]);
-
   return (
       <ContextProvider>
         <BrowserRouter>
